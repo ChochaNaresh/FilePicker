7b9cd5c34b6d8ba9daaf4daea77305a8d02f8545
NareshChocha <chochanaresh0@gmail.com>
2025-06-14 20:32:33 +0530

    add spotless and detekt plugins for code formatting and analysis
    - Add Spotless plugin for Kotlin code formatting.
    - Add Detekt plugin for static code analysis of Kotlin code.
    - Configure Spotless to use ktlint for Kotlin files and Kotlin Gradle scripts.
    - Configure Detekt with a custom configuration file.
    - Integrate Spotless and Detekt into the build process to run after Kotlin compilation.
    - Move Detekt configuration file to a top-level `config` directory.
    - Add `.editorconfig` to disable trailing commas and ignore certain annotations for function naming rules.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,13 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+import com.diffplug.gradle.spotless.SpotlessPlugin
+import io.gitlab.arturbosch.detekt.DetektPlugin
+import io.gitlab.arturbosch.detekt.extensions.DetektExtension
+import org.gradle.kotlin.dsl.dependencies
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.plugin.KotlinAndroidPluginWrapper
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
@@ -10,8 +20,41 @@ plugins {
     // code analyzer for Kotlin
     alias(libs.plugins.arturbosch.detekt) apply false
     alias(libs.plugins.maven.publish) apply false
+    alias(libs.plugins.spotless) apply false
 }
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.layout.buildDirectory)
+}
+val detektVersion = libs.versions.detekt.get()
+
+subprojects {
+    // formatting code for all subprojects
+    apply<SpotlessPlugin>()
+    configure<SpotlessExtension> {
+        kotlin {
+            target("src/**/*.kt")
+            targetExclude("build/**/*.kt")
+            ktlint()
+            endWithNewline()
+        }
+        kotlinGradle {
+            target("*.kts")
+            ktlint()
+        }
+    }
+    // code analysis for all subprojects
+    apply<DetektPlugin>()
+    configure<DetektExtension> {
+        toolVersion = detektVersion
+        config.from("$rootDir/config/detekt/detekt.yml")
+        buildUponDefaultConfig = true
+    }
+
+    afterEvaluate {
+        tasks.withType<KotlinCompile> {
+            finalizedBy("spotlessApply")
+            finalizedBy("detekt")
+        }
+    }
 }
